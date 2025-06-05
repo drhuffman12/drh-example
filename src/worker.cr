8972aa05f677f32cd8f9a76439c4ca1bfd5d1698
Daniel Huffman <drhuffman12@gmail.com>
2025-06-05 19:57:35 -0400

    update version

--- src/worker.cr
+++ src/worker.cr
@@ -1,6 +1,9 @@
 # TODO: Write documentation for `Drh::Example`
 module Drh::Example
-  VERSION = "0.1.0"
+  VERSION = "0.1.a"
 
   # TODO: Put your code here
+
+  p "Drh::Example version " + Drh::Example::VERSION
+
 end
