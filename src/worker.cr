94cf8e95e3874b46840a0325ea5429ff589ed953
Daniel Huffman <drhuffman12@gmail.com>
2025-06-05 21:42:06 -0400

    comment

--- src/worker.cr
+++ src/worker.cr
@@ -1,6 +1,6 @@
 # TODO: Write documentation for `Drh::Example`
 module Drh::Example
-  VERSION = "0.1.b"
+  # VERSION = "0.1.b"
 
   # TODO: Put your code here
   p "Drh::Example version " + Drh::Example::VERSION
